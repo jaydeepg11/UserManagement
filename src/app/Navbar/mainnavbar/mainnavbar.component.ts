04c84a65b2f2c9f5b2199b4d34163c0467c9f725
Jaydeepg11 <addjaydeep2011@gmail.com>
2024-07-13 14:19:59 +0530

    17:Hooks

--- src/app/Navbar/mainnavbar/mainnavbar.component.ts
+++ src/app/Navbar/mainnavbar/mainnavbar.component.ts
@@ -6,8 +6,8 @@ import { AfterContentInit, Component, ContentChild, ContentChildren, ElementRef,
   styleUrls: ['./mainnavbar.component.css']
 })
 export class MainnavbarComponent implements AfterContentInit {
-  @ContentChild('title')title!:ElementRef;
-  @ContentChildren('title')lsttitle!:QueryList<ElementRef>;
+  @ContentChild('title0')title!:ElementRef;
+  @ContentChildren('title0')lsttitle!:QueryList<ElementRef>;
 
 ngAfterContentInit(): void {
   console.log('Content Title : '+this.title.nativeElement.innerHTML);
--- src/app/contact/contact-us/contact-us.component.ts
+++ src/app/contact/contact-us/contact-us.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,15 +6,36 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent implements OnInit,OnChanges,DoCheck,AfterViewInit,AfterViewChecked,AfterContentInit,AfterContentChecked,OnDestroy {
   id:string="";
   constructor(private route : ActivatedRoute)
   {
-    
+    console.log('Contructor call...');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit call...');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked call...');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit call...');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked call...');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy call...');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('onchanges call...');
   }
   ngOnInit(): void {
     this.id = this.route.snapshot.params["id"];
-    
+    console.log('oninit call...');
+  }
+  ngDoCheck(): void {
+    console.log('Docheck call...');
   }
 
 
